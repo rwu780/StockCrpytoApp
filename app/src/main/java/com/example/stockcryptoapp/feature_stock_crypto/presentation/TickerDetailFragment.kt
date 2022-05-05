8ffd66be1cadc4a8fb412f7746ebc498b04a41cc
rwu780 <rwu4@ualberta.ca>
2022-05-05 14:19:40 -0400

    First Commit, Added dependencies, and generated empty fragments

---
+++ app/src/main/java/com/example/stockcryptoapp/feature_stock_crypto/presentation/TickerDetailFragment.kt
@@ -0,0 +1,17 @@
+package com.example.stockcryptoapp.feature_stock_crypto.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.stockcryptoapp.R
+
+class TickerDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/stockcryptoapp/feature_stock_crypto/presentation/TickerSearchFragment.kt
@@ -0,0 +1,16 @@
+package com.example.stockcryptoapp.feature_stock_crypto.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.stockcryptoapp.R
+
+
+class TickerSearchFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
